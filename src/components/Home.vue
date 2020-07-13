ec14983c63e80d5b531e41fc6358c39ff0fe24a2
mohammed-allou <60431576+mohammed-allou@users.noreply.github.com>
2020-07-13 17:05:42 +0100

    initialisation vue+vueRouter+vuetify

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ToolBar />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "./Footer";
+import ToolBar from "./ToolBar";
+
+export default {
+  components: { Footer, ToolBar }
+};
+</script>
